184e7964cd2531668bc66ede9cd73ce7536fd25c
edyting <dankyiemmanuel0@gmail.com>
2024-12-07 16:31:19 +0000

    working on the search feature

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import DashboardSkeleton from '../../ui/skeletons'
+
+const loading = () => {
+  return (
+    <div>
+      
+      <DashboardSkeleton/>
+    </div>
+  )
+}
+
+export default loading
